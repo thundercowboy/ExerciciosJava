f31f936af1c5a1a0ce92670fac5db84b55448ef8
Raphael Pedroso <raphael.pedroso97@hotmail.com>
2024-06-17 09:39:14 -0300

    Update

--- Exercicios/ExercicioGenerics3/application/Program.java
+++ Exercicios/ExercicioGenerics3/application/Program.java
@@ -1,32 +1,52 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
-		List<Shape> myShapes = new ArrayList<>();
-		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
+		Scanner sc = new Scanner(System.in);
 
-		List<Circle> myCircles = new ArrayList<>();
-		myCircles.add(new Circle(2.0));
-		myCircles.add(new Circle(3.0));
+		Map<String, Integer> votes = new LinkedHashMap<>();
 
-		System.out.println("Total area: " + totalArea(myCircles));
-	}
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
 
-	public static double totalArea(List<? extends Shape> list) {
-		double sum = 0.0;
-		for (Shape s : list) {
-			sum += s.area();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				
+				String[] fields = line.split(",");
+				String name = fields[0];
+				int count = Integer.parseInt(fields[1]);
+
+				if (votes.containsKey(name)) {
+					int votesSoFar = votes.get(name);
+					votes.put(name, count + votesSoFar);
+				}
+				else {
+					votes.put(name, count);
+				}
+				
+				line = br.readLine();
+			}
+			
+			for (String key : votes.keySet()) {
+				System.out.println(key + ": " + votes.get(key));
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
-		return sum;
+
+		sc.close();
 	}
 }
